a7eaaaa4894473a2337540755e8f3b3137e0bcca
Lokesh Singh <63910828+Lokesh598@users.noreply.github.com>
2022-03-30 08:21:28 +0530

    Experiments with templates and explicit specialization.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cassert>
+#include "practice.h"
+
+int main() {
+
+    testCout();
+    doMath();
+    showRandom();
+
+//    int age = getAge();
+//    printAge(age);
+
+    showTypeSizes();
+//    funWithEscaping();
+    narrowing();
+    typeCasting();
+    autoTyping();
+//    secondsToTime();
+    stringPractice();
+    makeStruct();
+    getToThePointer();
+    whatsYourVector();
+    rangeLoop();
+//    inputTests();
+    functionSignatures();
+    functionSignaturesMore();
+    candyTime();
+    testMaxN();
+
+    return EXIT_SUCCESS;
+}
